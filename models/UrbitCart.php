a7ae9dc0c6b3335e0b5c4d6bc4fef9e2af7862b0
amazing-cat <avkusnetsov@gmail.com>
2018-05-24 20:14:04 +0600

    remove GDPR data from urber_order_cart table after success send(cancel)

--- models/UrbitCart.php
+++ models/UrbitCart.php
@@ -7,6 +7,9 @@
  * @license Urbit
  */
 
+require_once(dirname(__FILE__) . '/../classes/api/UrbitShippingResponse.php');
+
+
 class UrbitCart
 {
     protected static $separator = '::';
@@ -370,13 +373,15 @@ class UrbitCart
 
     public static function updateResponseCode($responseCode, $urbitCartId)
     {
-        $ret = Db::getInstance()->execute(
-            'UPDATE ' . _DB_PREFIX_ . 'urbit_order_cart SET `response_code` ="' . pSQL($responseCode)
-            . '" WHERE `id_urbit_order_cart`=' . (int)$urbitCartId
-        );
 
-        return $ret;
+        if($responseCode == UrbitShippingResponse::HTTP_STATUS_SUCCESS_PUT ) {
+
+          return self::deleteUrbitCart($urbitCartId);
+        }
+
+        return 1;
     }
+
 
     public static function updateCheckoutId($checkoutId, $cartId)
     {
@@ -393,6 +398,15 @@ class UrbitCart
         $ret = Db::getInstance()->execute(
             'UPDATE ' . _DB_PREFIX_ . 'urbit_order_cart SET `preparation_end_time` ="' . pSQL($preparationEndTime)
             . '" WHERE `id_cart`=' . (int)$cartId
+        );
+
+        return $ret;
+    }
+
+    public static function deleteUrbitCart($urbitCartId)
+    {
+      $ret = Db::getInstance()->execute(
+          'DELETE FROM ' . _DB_PREFIX_ . 'urbit_order_cart WHERE `id_urbit_order_cart`=' . (int)$urbitCartId
         );
 
         return $ret;
